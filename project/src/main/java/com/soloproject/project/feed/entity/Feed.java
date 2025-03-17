5bfb88a421ab68692bce81960b9c35e9f34715da
minkawoo <minkawoo@gmail.com>
2025-03-17 19:23:11 +0900

    Entity

--- project/src/main/java/com/soloproject/project/feed/entity/Feed.java
+++ project/src/main/java/com/soloproject/project/feed/entity/Feed.java
@@ -2,20 +2,41 @@ package com.soloproject.project.feed.entity;
 
 import com.soloproject.project.photo.entity.Photo;
 import com.soloproject.project.user.entity.User;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Feed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long feedId;
+
+    @Column
     private String title;
+
+    @Column
     private String content;
+
+    @Column(nullable = false)
     LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false)
     LocalDateTime modifiedAt = LocalDateTime.now();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
-    @OneToMany
-    private Photo photo;
+    // 피드에 여러개 사진 올릴 수 있으니까
+    @OneToMany(mappedBy = "feed", cascade = CascadeType.ALL)
+    private List<Photo> photos = new ArrayList<>();
 }
