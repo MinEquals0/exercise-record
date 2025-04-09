e4d26531e0e53bb52e0282fc7cac7bcf1b30455b
minkawoo <minkawoo@gmail.com>
2025-04-09 16:14:21 +0900

    feat: complete user feature

--- project/src/main/java/com/soloproject/project/user/entity/User.java
+++ project/src/main/java/com/soloproject/project/user/entity/User.java
@@ -1,6 +1,5 @@
 package com.soloproject.project.user.entity;
 
-import com.soloproject.project.feed.entity.Feed;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,8 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -34,13 +31,32 @@ public class User {
     @Column
     private String bio;
 
-    @Column(nullable = false, name = "CREATED_AT")
-    private LocalDateTime createdAt = LocalDateTime.now();
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;; // 수정 시 변경 안됨
 
-    @Column(nullable = false, name = "MODIFIED_AT")
-    private LocalDateTime modifiedAt = LocalDateTime.now();
+    @Column(nullable = false)
+    LocalDateTime modifiedAt = LocalDateTime.now();
 
-    //한 사람이 여러개 피드 만들 수 있음
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private final List<Feed> feedList = new ArrayList<>();
+    //     새로운 엔티티 생성 시 createdAt과 modifiedAt 자동 설정
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = LocalDateTime.now();
+    }
+
+    // 엔티티 수정 시 modifiedAt이 자동으로 업데이트됨
+    @PreUpdate
+    protected void onUpdate(){
+        this.modifiedAt = LocalDateTime.now();
+    }
+
+//    @Column(nullable = false, name = "CREATED_AT")
+//    private LocalDateTime createdAt = LocalDateTime.now();
+//
+//    @Column(nullable = false, name = "MODIFIED_AT")
+//    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+//    //한 사람이 여러개 피드 만들 수 있음
+//    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//    private final List<Feed> feedList = new ArrayList<>();
 }
---
+++ project/src/main/java/com/soloproject/project/user/service/UserService.java
@@ -0,0 +1,48 @@
+package com.soloproject.project.user.service;
+
+import com.soloproject.project.user.entity.User;
+import com.soloproject.project.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    // 유저 생성
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    // 유저 수정
+    @Transactional
+    public User updateUser(Long userId, User updatedUser){
+        User existingUser = getUser(userId);
+        existingUser.setPassword(updatedUser.getPassword());
+        existingUser.setEmail(updatedUser.getEmail());
+        existingUser.setNickname(updatedUser.getNickname());
+        existingUser.setBio(updatedUser.getBio());
+        return existingUser;
+    }
+
+
+    // 유저 조회 - 단일
+    public User getUser(Long userId){
+        return userRepository.findById(userId)
+                .orElseThrow(()-> new RuntimeException("User not found with userId: " + userId));
+    }
+
+    // 유저 조회 - 단체
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // 유저 삭제
+    public void deleteUser(Long userId){
+        userRepository.deleteById(userId);
+    }
+}
