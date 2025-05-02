bdd5cb40cde9a36d463106e5a7b157b76299744a
MinEquals0 <minequals0@gmail.com>
2025-05-02 19:45:19 +0900

    fix: change TokenProvider

---
+++ project/src/main/java/com/soloproject/project/security/jwt/TokenProvider.java
@@ -0,0 +1,111 @@
+package com.soloproject.project.security.jwt;
+
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class TokenProvider implements InitializingBean {
+
+    private static final String AUTHORITIES_KEY = "auth";
+    private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
+
+    @Value("${jwt.secret}")
+    private String secret; // yml에서 secret 값을 받아옴
+    private long tokenValidityInMilliseconds; // yml에서 토큰 만료 시간을 받아옴
+    private Key key;
+
+
+    // 빈이 생성 되고 DI한 다음에 주입 받은 secret 값을
+    // Base64 Decode해서 key 변수에 할당
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+
+    }
+
+    // Authentication 객체에 있는 인증을 받은 유저 정보를 이용해 토큰을 생성
+    public String createToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        // yml에서 설정했던 토큰 만료시간을 설정
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+
+
+        // 토큰 생성해서 리턴
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(validity)
+                .compact();
+    }
+
+    // 토큰을 매개변수로 받아 토큰에 담겨있는 정보를 이용 Authentication객체 리턴하는 메소드
+    // 토큰에서 사용자 정보 가져오기
+    public Authentication getAuthentication(String token) {
+        // 토큰을 claims로 만들어 줌
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+
+        // claims에서 권한 정보들을 가져옴(authorities) -> 이걸 이용해 User 객체를 만듦
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        // User 정보, 토큰, 권한 정보를 이용해 최종적으로 Authentication 객체 리턴
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+
+    }
+
+    // 토큰을 매개변수로 받아 토큰의 유효성 검증을 수행하는 메소드
+    public boolean validateToken(String token) {
+        try {
+            // 매개변수로 받은 토큰을 파싱하여 발생하는 Exception들을 캐치해
+            // 문제가 있으면 return false, 없으면 true
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            logger.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            logger.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            logger.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            logger.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+
+    }
+
+
+}
