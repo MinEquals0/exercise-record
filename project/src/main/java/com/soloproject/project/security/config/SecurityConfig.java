3f21ba6b529df378e43e2a71ccb4eb3790da954d
MinEquals0 <minequals0@gmail.com>
2025-06-09 15:54:42 +0900

    refactor: rewrite SecurityConfig from scratch

--- project/src/main/java/com/soloproject/project/security/config/SecurityConfig.java
+++ project/src/main/java/com/soloproject/project/security/config/SecurityConfig.java
@@ -1,36 +1,57 @@
 package com.soloproject.project.security.config;
 
 
-import com.soloproject.project.security.jwt.JwtAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableMethodSecurity
-@RequiredArgsConstructor
+// HTTP 요청에 대한 보안 설정을 정의
+// 인증과 권한 부여, 예외 처리, 세션 관리 등을 처리하는 HTTP 요청을 구성하는 역할
+
+
+@Configuration// 설정 클래스, 스프링이 빈으로 등록
+@EnableWebSecurity // 스프링시큐리티를 활성화함
+@EnableMethodSecurity // 메서드 수준에서 권한 체크를 가능하게 해줌
+
 public class SecurityConfig {
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/user/signup").permitAll()
-                        .anyRequest().authenticated())
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+        http
+                // 토큰을 사용하니까 csrf는 disable
+                .csrf(AbstractHttpConfigurer::disable)
+                // 특정 API 경로는 인증 없이 접근 허용
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers("/api/hello",
+                                "/api/authenticate", // 로그인 API
+                                "/api/signup").permitAll() // 회원가입 API
+                        // 위의 API들은 토큰이 없는 상태에서 요청이 들어오기 때문에 모두 permitAll 설정
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()  // H2도 허용
+                        .anyRequest().authenticated()  // 그 외 요청은 인증 필요
+                )
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정 - 추가
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // H2 콘솔을 위한 설정 (iframe 허용)
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+
+                // 커스텀 JWT 보안 설정 적용 (JwtFilter 등록됨)
+                .with(new JwtSecurityConfig(tokenProvider), customizer -> {});
+
+        return http.build();
     }
 
     @Bean
